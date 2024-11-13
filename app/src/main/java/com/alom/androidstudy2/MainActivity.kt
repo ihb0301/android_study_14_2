7b266e667b2f7120c702e69e091e7c9d6761fe41
ihb0301 <abin0301@naver.com>
2024-11-13 21:26:43 +0900

    feat(MainActivity): 네트워크에서 목록 불러오는 기능 추가

--- app/src/main/java/com/alom/androidstudy2/MainActivity.kt
+++ app/src/main/java/com/alom/androidstudy2/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.alom.androidstudy2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.ReportFragment.Companion.reportFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.alom.androidstudy2.databinding.ActivityMainBinding
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,11 +30,39 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        var data : MutableList<ItemData> = mutableListOf(ItemData("1","갤럭시S24","1,000원","","1시간 전"),ItemData("2","맥북프로 16인치","100만원","","3일 전"))
+        val data : MutableList<ItemData> = mutableListOf()
 
         val adapter = RecyclerViewAdapter()
-        adapter.submitList(data)
         binding.mainRecyclerView.layoutManager = LinearLayoutManager(applicationContext, RecyclerView.VERTICAL, false)
         binding.mainRecyclerView.adapter = adapter
+
+        val jsonRequest = object : StringRequest(
+            Request.Method.POST,
+            "https://goaplrynweyxovekoezl.supabase.co/rest/v1/rpc/get_item7",
+            Response.Listener { response ->
+                val dataArray= JSONArray(JSONObject(response).getString("data"))
+                for (i in 0 until dataArray.length()) {
+                    val jsonObject = dataArray.getJSONObject(i)
+                    val item = ItemData(jsonObject.getString("id"),
+                        jsonObject.getString("title"),
+                        jsonObject.getString("price"),
+                        jsonObject.getString("image_url"),
+                        jsonObject.getString("time"))
+                    data.add(item)
+                    adapter.submitList(data)
+                }
+            },
+            Response.ErrorListener { var body = String(it.networkResponse.data, Charsets.UTF_8)
+                Log.d("","$body") }
+        ) {
+            override fun getHeaders(): MutableMap<String, String> {
+                var param = HashMap<String, String>()
+                param.put("apikey",BuildConfig.API_KEY)
+                return param
+            }
+        }
+        jsonRequest.setShouldCache(false)
+        val queue = Volley.newRequestQueue(this)
+        queue.add(jsonRequest)
     }
 }
